18d820912b79892d3e11a8846748a2bb10256fe6
Nishanta Paudel <nishantapaudel29@gmail.com>
2025-06-07 12:25:31 +0545

    done

---
+++ calc.py
@@ -0,0 +1,19 @@
+num1 = 20
+num2 = 3
+
+##Calculation
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2 
+div = num1 / num2
+intdiv = num1 // num2
+mod = num1 % num2
+cube = num1 ** 3
+
+print(f"The sum is {sum}")
+print(f"The difference is {diff}")
+print(f"The multiple is {mul}")
+print(f"The division is {div}")
+print(f"The division is {intdiv}")
+print(f"The mod is {mod}")
+print(f"The cube is {cube}")
