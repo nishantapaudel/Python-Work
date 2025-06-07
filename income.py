d0eead4146883e085eaff01c5e7aa806bb826fc4
Nishanta Paudel <nishantapaudel29@gmail.com>
2025-06-07 12:03:59 +0545

    commit

---
+++ income.py
@@ -0,0 +1,8 @@
+income = 2000
+expenses = 500
+
+balance = income - expenses
+
+print(f"Total income is {income}.")
+print(f"Total expenses is {expenses}.")
+print(f"Total Balance is {balance}.")
---
+++ variables.py
@@ -0,0 +1,11 @@
+name = "Nishanta Paudel"
+print(name)
+
+home = "Pokhara"
+print(f"My home is in {home}.")
+
+num1= 1
+num2= 2
+
+sum = num1 + num2 
+print (f"The sum is {sum}.")
